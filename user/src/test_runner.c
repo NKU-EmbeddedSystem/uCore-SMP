3360042009ac5bce3cd638b21805366f4c71d55c
zouxianyu <2979121738@qq.com>
2022-07-30 17:57:28 +0800

    在test_runner中拆分static和dynamic测试

--- user/src/test_runner.c
+++ user/src/test_runner.c
@@ -84,8 +84,7 @@ int main() {
     link("/libtls_get_new-dtv_dso.so", "/lib/libtls_get_new-dtv_dso.so");
     link("libtls_init_dso.so", "/lib/libtls_init_dso.so");
 
-    char *exe_file[] = {"entry-static.exe", "entry-dynamic.exe"};
-    char *testname[] = {
+    char *static_test[] = {
             "argv",
             "basename",
             "clocale_mbfuncs",
@@ -198,11 +197,131 @@ int main() {
             "wcsstr_false_negative"
     };
 
-    for (int i = 0; i < sizeof(exe_file) / sizeof(char *); i++) {
-        for (int j = 0; j < sizeof(testname) / sizeof(char *); j++) {
-            char *argv[] = {"runtest.exe", "-w", exe_file[i], testname[j], NULL};
-            test(argv);
-        }
+    char *dynamic_test[] = {
+            "argv",
+            "basename",
+            "clocale_mbfuncs",
+            "clock_gettime",
+            "crypt",
+            "dirname",
+            "dlopen",
+            "env",
+            "fdopen",
+            "fnmatch",
+            "fscanf",
+            "fwscanf",
+            "iconv_open",
+            "inet_pton",
+            "mbc",
+            "memstream",
+//            "pthread_cancel_points",
+//            "pthread_cancel",
+//            "pthread_cond",
+//            "pthread_tsd",
+            "qsort",
+            "random",
+            "search_hsearch",
+            "search_insque",
+            "search_lsearch",
+            "search_tsearch",
+            "sem_init",
+            "setjmp",
+            "snprintf",
+            "socket",
+            "sscanf",
+//            "sscanf_long",
+            "stat",
+            "strftime",
+            "string",
+            "string_memcpy",
+            "string_memmem",
+            "string_memset",
+            "string_strchr",
+            "string_strcspn",
+            "string_strstr",
+            "strptime",
+            "strtod",
+            "strtod_simple",
+            "strtof",
+            "strtol",
+            "strtold",
+            "swprintf",
+            "tgmath",
+            "time",
+            "tls_init",
+            "tls_local_exec",
+            "udiv",
+            "ungetc",
+            "utime",
+            "wcsstr",
+            "wcstol",
+            "daemon_failure",
+            "dn_expand_empty",
+            "dn_expand_ptr_0",
+            "fflush_exit",
+            "fgets_eof",
+//            "fgetwc_buffering",
+            "flockfile_list",
+            "fpclassify_invalid_ld80",
+            "ftello_unflushed_append",
+            "getpwnam_r_crash",
+            "getpwnam_r_errno",
+            "iconv_roundtrips",
+            "inet_ntop_v4mapped",
+            "inet_pton_empty_last_field",
+            "iswspace_null",
+            "lrand48_signextend",
+//            "lseek_large",
+            "malloc_0",
+            "mbsrtowcs_overflow",
+            "memmem_oob_read",
+            "memmem_oob",
+            "mkdtemp_failure",
+            "mkstemp_failure",
+            "printf_1e9_oob",
+            "printf_fmt_g_round",
+            "printf_fmt_g_zeros",
+            "printf_fmt_n",
+//            "pthread_robust_detach",
+//            "pthread_cond_smasher",
+//            "pthread_condattr_setclock",
+//            "pthread_exit_cancel",
+//            "pthread_once_deadlock",
+//            "pthread_rwlock_ebusy",
+            "putenv_doublefree",
+            "regex_backref_0",
+            "regex_bracket_icase",
+            "regex_ere_backref",
+            "regex_escaped_high_byte",
+            "regex_negated_range",
+            "regexec_nosub",
+            "rewind_clear_error",
+            "rlimit_open_files",
+            "scanf_bytes_consumed",
+            "scanf_match_literal_eof",
+            "scanf_nullbyte_char",
+            "setvbuf_unget",
+            "sigprocmask_internal",
+            "sscanf_eof",
+            "statvfs",
+            "strverscmp",
+            "syscall_sign_extend",
+            "tls_get_new_dtv",
+            "uselocale_0",
+            "wcsncpy_read_overflow",
+            "wcsstr_false_negative"
+    };
+
+    // static test
+    for (int i = 0; i < sizeof(static_test) / sizeof(char *); i++) {
+        char *argv[] = {"runtest.exe", "-w", "entry-static.exe", static_test[i], NULL};
+        test(argv);
+    }
+
+    // dynamic test
+    for (int i = 0; i < sizeof(dynamic_test) / sizeof(char *); i++) {
+        char *argv[] = {"runtest.exe", "-w", "entry-dynamic.exe", dynamic_test[i], NULL};
+        test(argv);
     }
 
     return 0;
