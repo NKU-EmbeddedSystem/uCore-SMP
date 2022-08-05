f1d8499899ef2511587cdd603c5c38c9924446bc
zouxianyu <2979121738@qq.com>
2022-08-05 18:41:24 +0800

    修复参数数组过小的bug

--- os/proc/exec.h
+++ os/proc/exec.h
@@ -2,7 +2,7 @@
 #define EXEC_H
 
 #define MAX_EXEC_ARG_COUNT 8
-#define MAX_EXEC_ARG_LENGTH 32
+#define MAX_EXEC_ARG_LENGTH 128
 
 int exec(char *name, int argc, const char **argv, int envc, const char **envp);
 
